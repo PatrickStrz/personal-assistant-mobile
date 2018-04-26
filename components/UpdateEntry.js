5ccb53bfcfba56ad0467e1bedd3156c53a7a74e2
Patrick Strzelec <patrick.strzelec@gmail.com>
2018-04-26 17:24:33 -0400

    started to add update

--- components/UpdateEntry.js
+++ components/UpdateEntry.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react"
 import { Mutation } from "react-apollo"
 import styled from "styled-components"
-import { Overlay } from "react-native-elements"
+import Overlay from "react-native-modal-overlay"
+import { TextInput } from "react-native-elements"
 
 const Text = styled.Text``
 
 export default class UpdateEntry extends Component {
   render() {
     return (
-      <Overlay isVisible>
+      <Overlay visible>
         <Text>Yoooooooo</Text>
       </Overlay>
     )
